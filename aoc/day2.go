33a40f710525a6d1031e657107923414f53502cc
Timm Dörr <timmdoerr509@gmail.com>
2024-12-09 18:54:05 +0100

    Day 2 Part 2

--- aoc/day2.go
+++ aoc/day2.go
@@ -1,7 +1,6 @@
 package aoc
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 )
@@ -57,11 +56,64 @@ var Day2_1 = Puzzle{
 				}
 			}
 			if isSafe {
-				fmt.Println(line)
 				safeCount += 1
 			}
 		}
 
 		return strconv.FormatInt(int64(safeCount), 10)
 	},
+}
+
+var Day2_2 = Puzzle{
+	Filepath: "inputs/day02.txt",
+	Solve: func(input string) string {
+		lines := strings.Split(input, "\n")
+
+		var safeCount int = 0
+		lines = lines[:len(lines)-1]
+
+		for _, line := range lines {
+			levelsStrings := strings.Split(line, " ")
+			levels := make([]int, 0, len(levelsStrings))
+
+			for _, number := range levelsStrings {
+				parsed, err := strconv.Atoi(number)
+				if err != nil {
+					panic(err)
+				}
+				levels = append(levels, parsed)
+			}
+
+			mergedLevels := make([]int, 0, len(levels))
+
+		Levels:
+			for i := range levels {
+				mergedLevels = mergedLevels[:0]
+				mergedLevels = append(mergedLevels, levels[:i]...)
+				mergedLevels = append(mergedLevels, levels[i+1:]...)
+				previous := mergedLevels[0]
+				previousSign := 0
+				for _, level := range mergedLevels[1:] {
+					if isUnsafeLevel(level, previous, previousSign) {
+						continue Levels
+					}
+					if level-previous < 0 {
+						previousSign = -1
+					} else if level-previous > 0 {
+						previousSign = 1
+					}
+					previous = level
+				}
+				safeCount++
+				break Levels
+			}
+		}
+
+		return strconv.FormatInt(int64(safeCount), 10)
+	},
+}
+
+func isUnsafeLevel(current int, previous int, previousSign int) bool {
+	// fmt.Printf("%v - %v = %v | %v\n", current, previous, current-previous, previousSign)
+	return (current-previous == 0) || ((current-previous > 0) && previousSign < 0) || ((current-previous < 0) && previousSign > 0) || (abs(current-previous) > 3)
 }
--- main.go
+++ main.go
@@ -5,6 +5,6 @@ import (
 )
 
 func main() {
-	aoc.Day2_1.LoadInput()
-	println(aoc.Day2_1.Solve(aoc.Day2_1.Input))
+	aoc.Day2_2.LoadInput()
+	println(aoc.Day2_2.Solve(aoc.Day2_2.Input))
 }
